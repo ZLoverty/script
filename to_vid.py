7bc2abd1458b7962b11729d9b28aa4d63e108ade
Zhengyang Liu <liuzy19911112@gmail.com>
2021-12-08 23:20:14 +0100

    add extract.py, to_vid check existence

--- to_vid.py
+++ to_vid.py
@@ -10,6 +10,7 @@ Edit:
 11242021 -- initial commit.
 11262021 -- Major change: now convert all the 8-bit folder in given folder to videos
             Change name to to_vid.py
+12082021 -- Check if the target .avi file exists already. If so, skip the conversion.
 """
 
 """SYNTAX
@@ -40,11 +41,14 @@ def main(folder, **kwargs):
             image_folder = os.path.split(i)[0]
             parent_folder, name = os.path.split(os.path.split(image_folder)[0]) # assume name/8-bit/*.tif
             output_file = os.path.join(parent_folder, "{}.avi".format(name))
-            input_imseq = os.path.join(image_folder, fmt)
-            cmd = 'ffmpeg -y -framerate {0:f} -i {1} -vcodec h264 {2}'.format(fps, input_imseq, output_file)
-            print("==============Start converting {} to video~~==============\n".format(name))
-            os.system(cmd)
-            print("\nConversion of {0} is successful!\nA video is saved at {1}\n\n".format(name, output_file))
+            if os.path.exists(output_file) == False:
+                input_imseq = os.path.join(image_folder, fmt)
+                cmd = 'ffmpeg -y -framerate {0:f} -i {1} -vcodec h264 {2}'.format(fps, input_imseq, output_file)
+                print("==============Start converting {} to video~~==============\n".format(name))
+                os.system(cmd)
+                print("\nConversion of {0} is successful!\nA video is saved at {1}\n\n".format(name, output_file))
+            else:
+                print("{} already exists, skipping ============".format(output_file))
 
 if __name__=="__main__":
     folder = sys.argv[1]
