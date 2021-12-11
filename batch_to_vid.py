328d3abde130091a4be5ef8d5d9d25f7fbebc82e
Zhengyang Liu <liuzy19911112@gmail.com>
2021-12-11 23:55:29 +0100

    Update batch_to_vid.py
    There's issue with to_vid.py and batch_to_vid.py. Need to reorganize.

--- batch_to_vid.py
+++ batch_to_vid.py
@@ -1,5 +1,6 @@
 import os
 from corrLib import readdata
+import sys
 
 """
 General
@@ -21,22 +22,30 @@ main_folder
 
 Usage
 =====
-Input the main_folder by editing the first line of the code and run the script by
-```
-python batch_to_vid.py
-```
+python batch_to_vid.py main_folder
+
 Test
 ====
-set the first line:
-```
-main_folder = os.path.join('test_images', 'batch_to_vid')
+python batch_to_vid.py test_images\batch_to_vid
+
+LOG
+===
+
+
+EDIT
+====
+Dec 11, 2021 -- add main_folder argument: it's better not to edit the script too often!
+                (issue) to_vid.py alone now performs the function of batch_to_vid.py. 
+                Although the work can be done, it's not a consistent design with the to_tif.
+                Need to work on this later. 
 """
-# main_folder = '/media/zhengyang/NothingToSay/DE/'
-main_folder = os.path.join('test_images', 'batch_to_vid')
-folders = next(os.walk(main_folder))[1]
-print(folders)
-for sf in folders:
-    folder = os.path.join(main_folder, sf)
-    cmd = "python to_vid.py {} fmt=8b%05d.tif fps=50".format(folder)
-    os.system(cmd)
+
+if __name__=="__main__":
+    main_folder = sys.argv[1]
+    folders = next(os.walk(main_folder))[1]
+    print(folders)
+    for sf in folders:
+        folder = os.path.join(main_folder, sf)
+        cmd = "python to_vid.py {} fmt=8b%05d.tif fps=50".format(folder)
+        os.system(cmd)
 
