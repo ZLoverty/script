6f3c5d2288f10f9c8e359a3b7a4211174225db89
Zhengyang Liu <liuzy19911112@gmail.com>
2022-02-21 18:01:18 +0100

    batch velocity autocorr script

---
+++ batch_velocity_autocorr.py
@@ -0,0 +1,63 @@
+import os
+import sys
+import time
+from corrLib import readdata
+
+"""
+GENERAL
+=======
+Batch velocity autocorrelation computation.
+
+USAGE
+=====
+python batch_mean_velocity.py main_piv_foler
+
+main_piv_folder is the folder containing many folders of PIV data. `piv_drop` for example.
+
+TEST
+====
+python batch_velocity_autocorr.py test_images\batch_spatial_correlation\piv_folder
+
+LOG
+===
+Mon Feb 21 17:58:32 2022
+------------------------
+Run batch_velocity_autocorr on test_images\batch_spatial_correlation\piv_folder
+Results will be saved in test_images\batch_spatial_correlation\velocity_autocorr
+The following files will be processed:
+        test_images\batch_spatial_correlation\piv_folder\00
+        test_images\batch_spatial_correlation\piv_folder\01
+------------------------
+
+Mon Feb 21 17:58:33 2022 // Computing VACF of test_images\batch_spatial_correlation\piv_folder\00
+C:\Users\liuzy\Documents\Github\Python\mylib\corrLib.py:706: RuntimeWarning: invalid value encountered in true_divide
+  corr = np.correlate(xn, xn, mode="same")[len(xn)//2:] / np.inner(xn, xn)
+Mon Feb 21 17:58:34 2022 // Computing VACF of test_images\batch_spatial_correlation\piv_folder\01
+C:\Users\liuzy\Documents\Github\Python\mylib\corrLib.py:706: RuntimeWarning: invalid value encountered in true_divide
+  corr = np.correlate(xn, xn, mode="same")[len(xn)//2:] / np.inner(xn, xn)
+
+EDIT
+====
+Feb 21, 2022 -- Initial commit.
+"""
+
+if __name__=="__main__":
+    main_piv_folder = sys.argv[1].rstrip(os.sep)
+    parent_folder = os.path.split(main_piv_folder)[0]
+    main_save_folder = os.path.join(parent_folder, "velocity_autocorr")
+    sfL = next(os.walk(main_piv_folder))[1]
+    if os.path.exists(main_save_folder) == False:
+        os.makedirs(main_save_folder)
+
+    print(time.asctime())
+    print("------------------------")
+    print("Run batch_velocity_autocorr on {}".format(main_piv_folder))
+    print("Results will be saved in {}".format(main_save_folder))
+    print("The following files will be processed:")
+    for sf in sfL:
+        print("\t{}".format(os.path.join(main_piv_folder, sf)))
+    print("------------------------")
+
+    for sf in sfL:
+        piv_folder = os.path.join(main_piv_folder, sf)
+        os.system("python velocity_autocorr.py {0} {1}".format(piv_folder, main_save_folder))
--- velocity_autocorr.py
+++ velocity_autocorr.py
@@ -35,62 +35,6 @@ EDIT
 Dec 13, 2021 -- Initial commit.
 Feb 21, 2022 -- Rewrite with new functions.
 """
-def autocorr_imseq(stack):
-    """Compute intensity autocorrelation of an image sequence.
-    Args:
-    stack -- A 3D array
-    Returns:
-    ac_mean -- the autocorrelation
-    Test:
-    stack = np.load(r'E:\moreData\08032020\small_imseq\06\stack.npy')[3000:3600]
-    ac = autocorr_imseq(stack)
-    plt.plot(np.arange(0, 600)/30, ac)
-    Edit:
-    Dec 13, 2021 -- Copy from corr_utils.py.
-    """
-    def autocorr(x):
-        x = (x - np.nanmean(x)) / np.nanstd(x)
-        result = np.correlate(x, x, mode='full')/len(x)
-        return result[len(result)//2:]
-    r = stack.reshape((stack.shape[0], stack.shape[1]*stack.shape[2])).transpose()
-    ac_list = []
-    for x in r:
-        ac = autocorr(x)
-        ac_list.append(ac)
-    ac_stack = np.stack(ac_list)
-    ac_mean = np.nanmean(ac_stack, axis=0)
-    return ac_mean
-
-def test():
-    piv_folder = sys.argv[1]
-    out_folder = sys.argv[2]
-    if os.path.exists(out_folder) == False:
-        os.makedirs(out_folder)
-    with open(os.path.join(out_folder, 'log.txt'), 'w') as f:
-        f.write(time.asctime() + " \\ Compute velocity autocorrelation function of PIV data in {}\n".format(piv_folder))
-        f.write(time.asctime() + " \\ The results will be saved in {}\n".format(out_folder))
-
-    l = readdata(piv_folder, "csv")
-    u_list = []
-    v_list = []
-    t_list = []
-    for num, i in l.iterrows():
-        X, Y, U, V = read_piv(i.Dir)
-        t = int(i.Name.split("-")[0])
-        u_list.append(U)
-        v_list.append(V)
-        t_list.append(t)
-    u = np.stack(u_list, axis=0)
-    v = np.stack(v_list, axis=0)
-    u_ac = autocorr_imseq(u)
-    v_ac = autocorr_imseq(v)
-    vac = (u_ac + v_ac) / 2
-    t = np.arange(len(vac)) * 2 # times 2 because PIV is done every 2 frames, unit: frame
-    vac_data = pd.DataFrame({"t": t, "vac":vac})
-    vac_data.to_csv(os.path.join(out_folder, "vac_data.csv"), index=False)
-
-    with open(os.path.join(out_folder, 'log.txt'), 'a') as f:
-        f.write(time.asctime() + " \\ Finish computing the VACF of {:d} velocity data".format(len(vac)))
 
 if __name__=="__main__":
     piv_folder = sys.argv[1]
@@ -104,6 +48,9 @@ if __name__=="__main__":
 
     if os.path.exists(out_folder) == False:
         os.makedirs(out_folder)
+
+    out_filename = os.path.split(piv_folder)[1] + ".csv"
+
     print(time.asctime() + " // Computing VACF of {}".format(piv_folder))
 
     ustack, vstack = read_piv_stack(piv_folder, cutoff=cutoff)
@@ -119,4 +66,4 @@ if __name__=="__main__":
     cv = vacf_piv(vstack, dt, mode="direct").rename(columns={"c": "vcorr"})
 
     vac_data = pd.concat((cu, cv), axis=1)
-    vac_data.to_csv(os.path.join(out_folder, "vac_data.csv"))
+    vac_data.to_csv(os.path.join(out_folder, out_filename))
