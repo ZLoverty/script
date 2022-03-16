d1afb2e76ff3daad29d27fae33261ba88dc3003e
Zhengyang Liu <liuzy19911112@gmail.com>
2022-03-16 13:40:43 +0100

    archive out-dated scripts and rewrite vacf script

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-positions = pd.read_csv(r"test_images\moving_mask_piv\positions.csv")
-name = "16"
-i = positions.loc[positions.Label=="{}.tif".format(name)]
-cmd = r"python moving_mask_piv.py test_images\moving_mask_piv\raw test_images\moving_mask_piv\piv_result 20 10 0.02 test_images\moving_mask_piv\mask.tif {0:d} {1:d} {2:d} {3:d}".format(int(i.X), int(i.Y), int(i.Major), int(i.Major))
-os.system(cmd)
--- velocity_autocorr.py
+++ velocity_autocorr.py
@@ -1,25 +1,20 @@
 import pandas as pd
-from corrLib import readdata, autocorr_t, vacf_piv
-from myImageLib import bpass
-from skimage import io
-import numpy as np
 import sys
 import os
 import time
-from pivLib import read_piv_stack, read_piv
-import scipy
-import pdb
+from pivLib import piv_data
+from myImageLib import readdata
 
 """
 GENERAL
 =======
 Apply autocorrelation analysis on sequential PIV data.
-Modified based on `autocorr_imseq.py`.
 
 USAGE
 =====
-python velocity_autocorr.py piv_folder out_folder [dt] [cutoff]
+python velocity_autocorr.py piv_folder out_folder [fps=50] [cutoff=250]
 
+fps is the imaging frame rate and the dt between two adjacent frames is assumed to be 2 / fps
 cutoff is set to analysis only the initial part of the velocity data.
 
 TEST
@@ -34,12 +29,15 @@ EDIT
 ====
 Dec 13, 2021 -- Initial commit.
 Feb 21, 2022 -- Rewrite with new functions.
+Mar 16, 2022 -- 1. Rewrite with piv_data class
+                2. Use fps instead of dt as input argument
+                3. Modify doc string
 """
 
 if __name__=="__main__":
     piv_folder = sys.argv[1]
     out_folder = sys.argv[2]
-    dt = 0.04 # fps=50, dt = 2/fps
+    fps = 50
     cutoff = 250
     if len(sys.argv) > 3:
         dt = float(sys.argv[3])
@@ -48,22 +46,36 @@ if __name__=="__main__":
 
     if os.path.exists(out_folder) == False:
         os.makedirs(out_folder)
+        
+    l = readdata(piv_folder, "csv")
+    piv = piv_data(l, fps=fps)
+    ac = piv.vacf()
 
     out_filename = os.path.split(piv_folder)[1] + ".csv"
+    ac.to_csv(os.path.join(out_folder, out_filename))
 
-    print(time.asctime() + " // Computing VACF of {}".format(piv_folder))
 
-    ustack, vstack = read_piv_stack(piv_folder, cutoff=cutoff)
 
-    # smooth the velocity fields with gaussian filter (4*sigma=3)
 
-    ustack = scipy.ndimage.gaussian_filter(ustack, (3/4,0,0))
-    vstack = scipy.ndimage.gaussian_filter(vstack, (3/4,0,0))
 
-    # The two step can potentially be combined by using 4-D arrays
-
-    cu = vacf_piv(ustack, dt, mode="direct").rename(columns={"c": "ucorr"})
-    cv = vacf_piv(vstack, dt, mode="direct").rename(columns={"c": "vcorr"})
-
-    vac_data = pd.concat((cu, cv), axis=1)
-    vac_data.to_csv(os.path.join(out_folder, out_filename))
+    # if os.path.exists(out_folder) == False:
+    #     os.makedirs(out_folder)
+    #
+    # out_filename = os.path.split(piv_folder)[1] + ".csv"
+    #
+    # print(time.asctime() + " // Computing VACF of {}".format(piv_folder))
+    #
+    # ustack, vstack = read_piv_stack(piv_folder, cutoff=cutoff)
+    #
+    # # smooth the velocity fields with gaussian filter (4*sigma=3)
+    #
+    # ustack = scipy.ndimage.gaussian_filter(ustack, (3/4,0,0))
+    # vstack = scipy.ndimage.gaussian_filter(vstack, (3/4,0,0))
+    #
+    # # The two step can potentially be combined by using 4-D arrays
+    #
+    # cu = vacf_piv(ustack, dt, mode="direct").rename(columns={"c": "ucorr"})
+    # cv = vacf_piv(vstack, dt, mode="direct").rename(columns={"c": "vcorr"})
+    #
+    # vac_data = pd.concat((cu, cv), axis=1)
+    # vac_data.to_csv(os.path.join(out_folder, out_filename))
