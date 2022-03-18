d22253aa10b21832d9d12af3bc35e01b03dd69db
Zhengyang Liu <liuzy19911112@gmail.com>
2022-03-18 10:01:54 +0100

    remove main check

--- batch_velocity_autocorr.py
+++ batch_velocity_autocorr.py
@@ -36,24 +36,24 @@ EDIT
 Feb 21, 2022 -- Initial commit.
 """
 
-if __name__=="__main__":
-    main_piv_folder = sys.argv[1].rstrip(os.sep)
-    parent_folder = os.path.split(main_piv_folder)[0]
-    main_save_folder = os.path.join(parent_folder, "velocity_autocorr")
-    sfL = next(os.walk(main_piv_folder))[1]
-    if os.path.exists(main_save_folder) == False:
-        os.makedirs(main_save_folder)
 
-    print(time.asctime())
-    print("------------------------")
-    print("Run batch_velocity_autocorr on {}".format(main_piv_folder))
-    print("Results will be saved in {}".format(main_save_folder))
-    print("The following files will be processed:")
-    for sf in sfL:
-        print("\t{}".format(os.path.join(main_piv_folder, sf)))
-    print("------------------------")
+main_piv_folder = sys.argv[1].rstrip(os.sep)
+parent_folder = os.path.split(main_piv_folder)[0]
+main_save_folder = os.path.join(parent_folder, "velocity_autocorr")
+sfL = next(os.walk(main_piv_folder))[1]
+if os.path.exists(main_save_folder) == False:
+    os.makedirs(main_save_folder)
 
-    for sf in sfL:
-        piv_folder = os.path.join(main_piv_folder, sf)
-        print(time.asctime() + " Computing velocity autocorrelation of {}".format(os.path.join(main_piv_folder, sf)))
-        os.system("python velocity_autocorr.py {0} {1}".format(piv_folder, main_save_folder))
+print(time.asctime())
+print("------------------------")
+print("Run batch_velocity_autocorr on {}".format(main_piv_folder))
+print("Results will be saved in {}".format(main_save_folder))
+print("The following files will be processed:")
+for sf in sfL:
+    print("\t{}".format(os.path.join(main_piv_folder, sf)))
+print("------------------------")
+
+for sf in sfL:
+    piv_folder = os.path.join(main_piv_folder, sf)
+    print(time.asctime() + " Computing velocity autocorrelation of {}".format(os.path.join(main_piv_folder, sf)))
+    os.system("python velocity_autocorr.py {0} {1}".format(piv_folder, main_save_folder))
