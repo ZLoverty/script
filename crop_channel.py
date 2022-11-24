0e844b7aa55eef65dfb9382d4c9f254a0ac45998
Zhengyang Liu <liuzy19911112@gmail.com>
2022-11-24 12:22:47 +0100

    Update readme and rebuild documentation

--- crop_channel.py
+++ crop_channel.py
@@ -47,7 +47,7 @@ The folder structure is illustrated below:
 .. rubric:: Edit
 
 :Nov 03, 2022: Initial commit.
-:Nov 23, 2022: To make the workflow consistent with Claire's, it is more convenient to crop the channel region directly from the *.nd2 file, and save as tifstack. Now crop directly from \*.nd2 files to  tifstack.
+:Nov 23, 2022: To make the workflow consistent with Claire's, it is more convenient to crop the channel region directly from the \*.nd2 file, and save as tifstack. Now crop directly from \*.nd2 files to  tifstack.
 """
 
 crop_data_dir = sys.argv[1]
