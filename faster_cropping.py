73344c9d09ab1d3cf807f2c05d9c47a9caa530fa
Zhengyang Liu <liuzy19911112@gmail.com>
2023-03-31 16:47:55 +0200

    output preview and crop indicator

--- faster_cropping.py
+++ faster_cropping.py
@@ -5,12 +5,17 @@ from matplotlib.patches import Rectangle
 import numpy as np
 import os
 import pandas as pd
+from scipy.ndimage import map_coordinates
+from tifffile import imwrite
+from myimagelib.myImageLib import to8bit
 
 ### Required settings below
 
-imgDir = r"test_images\faster_cropping\crop-test.tif"
+imgDir = r"A:\SYMMETRIC\28 feb 2023\nd2\06.nd2"
 
-save_folder = r"test_images\faster_cropping"
+save_folder = r"A:\SYMMETRIC\28 feb 2023"
+
+suffix = 2
 
 angles = [0, 120, 240]
 
@@ -18,13 +23,20 @@ w, h = 400, 500
 
 ### Settings above
 
-img = io.imread(imgDir)
+if imgDir.endswith(".nd2"):
+    from nd2reader import ND2Reader
+    with ND2Reader(imgDir) as images:
+        img = images[0]
+elif imgDir.endswith(".tif"):
+    img = io.imread(imgDir)
+    if len(img.shape) == 3:
+        img = img[0]
 
 colors = plt.get_cmap("tab10")
 
 fig, ax = plt.subplots()
 
-ax.imshow(img, cmap="gray")
+ax.imshow(to8bit(img), cmap="gray")
 
 # patch = Rectangle((0, 0), 500, 300, fill=False, lw=5)
 
@@ -44,7 +56,6 @@ class RectInteractor:
         canvas = self.ax.figure.canvas
 
         # self.ax.add_patch(rectpatch)
-        self.status = 0
         self.lines, self.rects = {}, {}
         self.xy, self.angles = {}, {} # rectangle base points
         self.numChannels = len(angles)
@@ -55,7 +66,6 @@ class RectInteractor:
 
         canvas.mpl_connect('draw_event', self.on_draw)
         canvas.mpl_connect("button_press_event", self.on_button_press)
-        canvas.mpl_connect("button_release_event", self.on_button_release)
         canvas.mpl_connect("motion_notify_event", self.on_mouse_move)
         canvas.mpl_connect("key_press_event", self.on_key_press)
 
@@ -72,8 +82,7 @@ class RectInteractor:
     def on_button_press(self, event):
         """Callback for mouse button presses."""
         if (event.inaxes is None or 
-            event.button != MouseButton.LEFT or 
-            self.status != 0):
+            event.button != MouseButton.LEFT):
             return
         self.xc = np.array((event.xdata, event.ydata))
 
@@ -126,28 +135,20 @@ class RectInteractor:
             # image center, for rotating base points
             # this is to mimic the manual cropping in ImageJ
             xc = np.array((img.shape[1]//2, img.shape[0]//2))
-            # the saved base points should be the diagonally opposite vertices
-            xy = {}
-            for i in range(self.numChannels):
-                xy[i] = self.xy[i] + self.h_unit[i] * h + self.w_unit[i] * w
-                xy[i] = np.matmul(self.rotation_matrix(180-self.angles[i]), xy[i] - xc) + xc 
 
-            print(self.xy[0])
-            print(self.h_unit[0])
-            print(self.w_unit[0])
-            
-            df1 = pd.DataFrame(np.stack([xy[i] for i in xy]), columns=["BX", "BY"], index=[1,3,5]).assign(Width=w, Height=h).astype("int")
-            df2 = pd.DataFrame(270-np.array([self.angles[i] for i in self.angles]), columns=["Angle"], index=[0,2,4])
-            pd.concat([df1, df2]).sort_index().to_csv(os.path.join(save_folder, "crop_data_test.csv"))
-
-    def on_button_release(self, event):
-        """Callback for mouse button releases."""
-        if (event.button != MouseButton.LEFT):
-            return
-        if self.status == 2:
-            self.status = 0
+            dfxy = pd.DataFrame(np.stack([self.xy[i] for i in self.xy]), columns=["BX", "BY"], index=range(self.numChannels))
+            dfhu = pd.DataFrame(np.stack([self.h_unit[i] for i in self.h_unit]), columns=["hu0", "hu1"], index=range(self.numChannels))
+            dfwu = pd.DataFrame(np.stack([self.w_unit[i] for i in self.w_unit]), columns=["wu0", "wu1"], index=range(self.numChannels))
+            df = dfxy.join(dfhu).join(dfwu).assign(h=h, w=w)
+            df.to_csv(os.path.join(save_folder, "crop_data{}.csv".format(suffix)))
+            fig.savefig(os.path.join(save_folder, "crop{}.jpg".format(suffix)))
+            for i in self.xy:
+                YY, XX = np.mgrid[0:h, 0:w]
+                YY = np.flip(YY, axis=0)
+                X = self.xy[i][0] + XX * self.w_unit[i][0] + YY * self.h_unit[i][0]
+                Y = self.xy[i][1] + XX * self.w_unit[i][1] + YY * self.h_unit[i][1]
+                crop = map_coordinates(img, [Y, X], order=0, mode="constant")
+                imwrite(os.path.join(save_folder, "{0}{1}.tif".format(chr(65+i), suffix)), to8bit(crop))
 
 interactor = RectInteractor(ax)
-# ax.set_xlim([-10, 10])
-# ax.set_ylim([-10, 10])
 plt.show()
