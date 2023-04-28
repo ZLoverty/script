4feea710b315ff15b3733ac1803b60e94c2e2a26
ZLoverty <liuzy19911112@gmail.com>
2023-04-28 16:31:56 +0200

    update copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,7 @@ sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 # -- Project information -----------------------------------------------------
 
 project = 'script'
-copyright = '2022, Zhengyang Liu'
+copyright = '2022-2023, Zhengyang Liu'
 author = 'Zhengyang Liu'
 
 # The full version, including alpha/beta/rc tags
