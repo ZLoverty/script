967204774c6cbbcf88a5e737c99762915354c218
ZLoverty <liuzy19911112@gmail.com>
2023-04-28 17:14:01 +0200

    version matters?

--- small_imseq.py
+++ small_imseq.py
@@ -1,7 +1,4 @@
 """
-small_imseq
-===========
-
 Concentration has always been derived from the bright field images. While images are typically 1280*1080, the data we use are always coarse-grained down to 50*42. Although the down sizing step takes only several milliseconds, repeating the process over and over for hundreds of thousands of times (all images) takes a lot of time. Thus, I decide to convert the image sequences to the coarse-grained version and save them as numpy binary data. In this way, the 3000 frame video will be down sized to 3000*50*42 float16 numbers, which in total is 6M instead of the 4.64G. By saving this down sized data, the data also gets more portable because of the small size, while all the useful information is retained.
 
 
